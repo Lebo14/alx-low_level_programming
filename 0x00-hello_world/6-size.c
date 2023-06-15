d1660319aae7eedf1b9af537d4364719ff18f56c
Lebo14 <lebohanglekitlane8@gmail.com>
2023-06-15 07:22:17 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,10 @@
 #include <stdio.n>
 /**
  * main - A program that prints the sizes of various computer types
- * Return: 0 (Success)
+ * Return: always 0
  */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
 printf("size of a char: %zu byte(s)\n", (unsigned long, sizeof(a));
 printf("size of a int: %zu byte(s)\n", (unsigned long, sizeof(b));
 printf("size of a long int: %zu byte(s)\n", (unsigned long, sizeof(c));
