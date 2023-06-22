fd9ed6f789cf6242d55a3782e8a47f209dbd523a
Lebo14 <lebohanglekitlane8@gmail.com>
2023-06-22 09:43:04 -0700

    Task 2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,4 +5,4 @@ void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int year, int day);
-#endif /*MAIN_H */
+#endif/*MAIN_H*/ 
