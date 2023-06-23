2d2160b2da83e6ef7b48decd7c30a66c0735b524
Lebo14 <lebohanglekitlane8@gmail.com>
2023-06-23 02:03:39 -0700

    Task 4

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,12 +3,12 @@
  * print_numbers - Print the number since 0 up to 9
  * Return: The number since 0 up to 9
  */
-void print_numbers(void);
+void print_numbers(void)
 {
 	int x;
-	for (x = 0, x <= 9; x++)
+	for (x = 0; x <= 9; x++)
 	{
-	_putchar(x + '0');
+		_putchar(x + '0');
 	}
-_putchar('\n');
+	_putchar('\n');
 }
