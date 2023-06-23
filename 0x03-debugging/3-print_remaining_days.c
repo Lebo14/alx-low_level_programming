5bddf77909d2c3f2c6477ae6635a95fc98138525
Lebo14 <lebohanglekitlane8@gmail.com>
2023-06-23 14:12:19 -0700

    Task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include "main.h"
-/** 
- * print_remaining_days - takes a date and print 
+
+/**
+ * print_remaining_days - takes a date and print
+ *
  * how many days are there
  * @month: month in number format
  * @day: day of month
  * @year: year
  * Return: void
  */
+
 void print_remaining_days(int month, int day, int year)
+
 {
 	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
