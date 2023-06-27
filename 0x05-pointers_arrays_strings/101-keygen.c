f8e121ce17ae2969ccbd054f474d489ed711df93
Lebo14 <lebohanglekitlane8@gmail.com>
2023-06-26 20:59:02 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,13 +11,13 @@ int main(void)
 	char password[84];
 	int index = 0, sum = 0, diff_half1, diff_half2;
 
-	srang(time(0));
+	srand(time(0));
 
 	while (sum < 2772)
 
 	{
 		password[index] = 33 + rand() % 94;
-		sum += pasword[index++];
+		sum += password[index++];
 	}
 	password[index] = '\0';
 
